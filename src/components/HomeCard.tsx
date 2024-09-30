9a437f8da0fc899abf99a6c0f3c3b46b40c8ab4a
harshgaggar19 <harshgaggar19@gmail.com>
2024-09-30 19:51:00 +0530

    signup page for college and inspector uploaded

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -12,16 +12,17 @@ export function HomeCard({ Role, Content,url }: HomeCardProps) {
 			<div
 				className={cn(
 					"group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-					`bg-[url(${url})] bg-cover`,
+					`bg-[url(${url})] bg-cover bg-center `,
 					// Preload hover image by setting it in a pseudo-element
 					`before:bg-[url(${url})] before:fixed before:inset-0 before:opacity-0 before:z-[-1]`,
 					`hover:bg-[url(${url})]`,
 					"hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50 hover:text-white",
 					"transition-all duration-500  rounded-3xl shadow-2xl shadow-slate-600"
 				)}
+				
 			>
 				<div className="text relative z-50 ">
-					<h1 className="font-bold text-xl md:text-3xl relative">{Role}</h1>
+					<h1 className="font-bold text-xl md:text-3xl relative ">{Role}</h1>
 					<p className="font-normal text-base relative my-4">{Content}</p>
 				</div>
 			</div>
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -3,11 +3,6 @@ import React from "react";
 import { Label } from "../../@/lib/ui/label";
 import { Input } from "../../@/lib/ui/input";
 import { cn } from "../../@/lib/utils";
-import {
-	IconBrandGithub,
-	IconBrandGoogle,
-	IconBrandOnlyfans,
-} from "@tabler/icons-react";
 
 export function SignupFormDemo() {
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -21,53 +16,61 @@ export function SignupFormDemo() {
 					alt="#"
 					className="-z-10 absolute object-cover h-full w-full"
 				/>
-				<div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-					<h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-						Welcome to InspAIre
-					</h2>
-					
-
-					<form className="my-8" onSubmit={handleSubmit}>
-						<div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-							<LabelInputContainer>
-								<Label htmlFor="firstname">First name</Label>
-								<Input id="firstname" placeholder="Tyler" type="text" />
-							</LabelInputContainer>
-							<LabelInputContainer>
-								<Label htmlFor="lastname">Last name</Label>
-								<Input id="lastname" placeholder="Durden" type="text" />
-							</LabelInputContainer>
+				<div className="flex items-center justify-center p-5">
+					<div className="relative rounded-lg h-[88vh] max-w-[80vw] w-full bg-white p-10 shadow-lg">
+						<div className="absolute top-0 left-1/2 h-full w-1/2 z-[99] hidden lg:block">
+							<div className="absolute top-0 left-0 h-full w-full z-[10]">
+								<div className="absolute rounded-r-md top-0 left-0 h-full w-full bg-black opacity-50 z-[12]" />
+								<img
+									src="https://images.unsplash.com/photo-1577546568088-eb32790be7ec?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+									alt="Logo2"
+									className="absolute h-full w-full object-cover rounded-r-lg"
+								/>
+							</div>
 						</div>
-						<LabelInputContainer className="mb-4">
-							<Label htmlFor="email">Email Address</Label>
-							<Input
-								id="email"
-								placeholder="example@fc.com"
-								type="email"
-							/>
-						</LabelInputContainer>
-						<LabelInputContainer className="mb-4">
-							<Label htmlFor="password">Password</Label>
-							<Input id="password" placeholder="••••••••" type="password" />
-						</LabelInputContainer>
-						<LabelInputContainer className="mb-8">
-							<Label htmlFor="twitterpassword">Confirm your password</Label>
-							<Input
-								id="twitterpassword"
-								placeholder="••••••••"
-								type="twitterpassword"
-							/>
-						</LabelInputContainer>
+						<div className=" w-1/2 absolute right-1/2 mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+							<h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+								Welcome to InspAIre
+							</h2>
 
-						<button
-							className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-							type="submit"
-						>
-							Sign up &rarr;
-							<BottomGradient />
-						</button>
+							<form className="my-8" onSubmit={handleSubmit}>
+								<div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+									<LabelInputContainer>
+										<Label htmlFor="firstname">First name</Label>
+										<Input id="firstname" placeholder="Tyler" type="text" />
+									</LabelInputContainer>
+									<LabelInputContainer>
+										<Label htmlFor="lastname">Last name</Label>
+										<Input id="lastname" placeholder="Durden" type="text" />
+									</LabelInputContainer>
+								</div>
+								<LabelInputContainer className="mb-4">
+									<Label htmlFor="email">Email Address</Label>
+									<Input id="email" placeholder="example@fc.com" type="email" />
+								</LabelInputContainer>
+								<LabelInputContainer className="mb-4">
+									<Label htmlFor="password">Password</Label>
+									<Input id="password" placeholder="••••••••" type="password" />
+								</LabelInputContainer>
+								<LabelInputContainer className="mb-8">
+									<Label htmlFor="twitterpassword">Confirm your password</Label>
+									<Input
+										id="twitterpassword"
+										placeholder="••••••••"
+										type="twitterpassword"
+									/>
+								</LabelInputContainer>
 
-					</form>
+								<button
+									className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+									type="submit"
+								>
+									Sign up &rarr;
+									<BottomGradient />
+								</button>
+							</form>
+						</div>
+					</div>
 				</div>
 			</>
 		);
